9a193ef75901297bdefb91894963949c7ee60511
이정재 ( Lee Jeong Jae ) <cssopy@gmail.com>
2022-11-30 08:44:18 +0900

    Update BJ_1152.java

--- BAEKJOON/BRONZE_2/BJ_1152.java
+++ BAEKJOON/BRONZE_2/BJ_1152.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 단어의 개수 (브론즈 2)
+// 단어의 개수
 // 한줄에 여러 영어단어들이 공백으로 구분되어 있는데 이 영어단어의 개수를 구하는 문제
 // https://www.acmicpc.net/problem/1152
 public class BJ_1152 {
