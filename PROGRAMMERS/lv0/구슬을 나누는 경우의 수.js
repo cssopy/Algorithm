78d68923b7412feeeb16e3879c5a4810d7e07b12
cssopy <83584721+cssopy@users.noreply.github.com>
2023-02-04 14:18:35 +0900

    Create 구슬을 나누는 경우의 수.js

---
+++ PROGRAMMERS/lv0/구슬을 나누는 경우의 수.js
@@ -0,0 +1,14 @@
+let fac = (n) => (n == 1 ? 1 : n * fac(n - 1));
+
+function solution(balls, share) {
+    return Math.round(fac(balls) / fac(balls - share) / fac(share));
+}
+
+let inps = [
+    [3, 2],
+    [5, 3],
+];
+
+for (let i of inps) {
+    console.log(solution(i[0], i[1]));
+}
