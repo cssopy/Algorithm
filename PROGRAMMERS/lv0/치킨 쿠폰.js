19a13407289930ea872bc56ccd0f214968326288
cssopy <83584721+cssopy@users.noreply.github.com>
2023-02-21 22:44:19 +0900

    Create 치킨 쿠폰.js

---
+++ PROGRAMMERS/lv0/치킨 쿠폰.js
@@ -0,0 +1,15 @@
+function solution(chicken) {
+    let ans = 0;
+    while (parseInt(chicken / 10) != 0) {
+        ans += parseInt(chicken / 10);
+        chicken = parseInt(chicken / 10) + chicken % 10;
+    }
+    return ans;
+}
+
+let inps = [
+    100,
+    1081 
+]
+
+inps.forEach(v => console.log(solution(v)));
