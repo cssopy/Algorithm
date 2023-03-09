a44c54f0c896a89de7c2379043c975170c3a5700
cssopy <83584721+cssopy@users.noreply.github.com>
2023-03-09 12:09:57 +0900

    Update lv2_올바른_괄호.java

--- PROGRAMMERS/lv2/lv2_올바른_괄호.java
+++ PROGRAMMERS/lv2/lv2_올바른_괄호.java
@@ -17,28 +17,28 @@ public class lv2_올바른_괄호 {
     }
 
     static boolean solution(String s) {
-        int top = -1;
+        int sl = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            // 여는 괄호이면 스택에 여는 괄호를 넣었다치고 top++ 수행
+            // 여는 괄호이면 스택에 여는 괄호를 넣었다치고 sl++ 수행
             if (c == '(') {
-                top++;
+                sl++;
                 continue;
             }
 
             if (c == ')') {
-                // 닫는 괄호가 나올때 top은 -1이 아니어야 함
-                if (top == -1) {
+                // 닫는 괄호가 나올때 sl은 0이 아니어야 함
+                if (sl == 0) {
                     return false;
                 }
 
-                top--;
+                sl--;
             }
         }
 
         // 정상적으로 진행하다 스택에 여는 괄호가 남아있는 경우가 존재하기에 처리
-        return top != -1 ? false : true;
+        return sl != 0 ? false : true;
     }
 }
