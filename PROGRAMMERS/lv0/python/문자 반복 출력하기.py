8ec7af5c79420a0b29db64dc987251a7024e9953
이정재 ( Lee Jeong Jae ) <83584721+cssopy@users.noreply.github.com>
2023-04-13 17:43:44 +0900

    delete python files

--- PROGRAMMERS/lv0/python/문자 반복 출력하기.py
+++
@@ -1,9 +0,0 @@
-def solution(my_string, n):
-    return ''.join(i*n for i in my_string)
-
-inps = [
-    ["hello", 3]
-]
-
-for (s, n) in inps:
-    print(solution(s, n))
--- PROGRAMMERS/lv0/python/직각삼각형 출력하기.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-
-for i in range(3):
-    print('*' * (i+1))
