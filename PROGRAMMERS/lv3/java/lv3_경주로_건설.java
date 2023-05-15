d56038e35a9f603ced2b5da3171b1ff7cacbd3bc
이정재 ( Lee Jeong Jae ) <83584721+cssopy@users.noreply.github.com>
2023-05-15 11:14:16 +0900

    Create lv3_경주로_건설.java

---
+++ PROGRAMMERS/lv3/java/lv3_경주로_건설.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class lv3_경주로_건설 {
+    public static void main(String[] args) {
+        StringBuilder result = new StringBuilder();
+
+        int[][][] inps = {
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 1}, {0, 0, 1, 0, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 1, 0, 0}, {1, 0, 0, 0, 0, 0, 0, 0}},
+                {{0, 0, 1, 0}, {0, 0, 0, 0}, {0, 1, 0, 1}, {1, 0, 0, 0}},
+                {{0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 0}, {0, 0, 1, 0, 0, 0}, {1, 0, 0, 1, 0, 1}, {0, 1, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 0}}
+        };
+
+        for (int[][] inp : inps) {
+            result.append(String.format("%d\n", solution(inp)));
+        }
+
+        System.out.println(result);
+    }
+
+    public static int solution(int[][] board) {
+        Queue<Point> q = new LinkedList<>();
+        int[][][] costTable = new int[4][board.length][board.length];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < board.length; j++) {
+                Arrays.fill(costTable[i][j], Integer.MAX_VALUE);
+            }
+        }
+
+        q.add(new Point(0, 0, 0, -1));
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int answer = Integer.MAX_VALUE;
+        while (!q.isEmpty()) {
+            Point cp = q.poll();
+
+            // 도착하여도 이후에 더 적은 비용이 존재할 수 있으므로 계속 진행
+            if (cp.r == cp.c && cp.r == board.length - 1) {
+                answer = Math.min(answer, cp.cost);
+                continue;
+            }
+
+            for (int dir = 0; dir < dirs.length; dir++) {
+                int nr = cp.r + dirs[dir][0];
+                int nc = cp.c + dirs[dir][1];
+
+                // 다음 좌표가 맵 밖이거나 벽이면 continue
+                if (!(0 <= nr && nr < board.length && 0 <= nc && nc < board.length)) continue;
+                if (board[nr][nc] == 1) continue;
+
+                // 다음 좌표 비용 계산
+                int ncost = cp.cost;
+                if (dir == cp.dir || cp.dir == -1) ncost += 100;
+                else ncost += 600;
+
+                // 앞전에 비용이 결정된 것보다 더 큰 비용이 들어왔을 때는 무시, 방문여부의 역할도 가능
+                if (ncost > costTable[dir][nr][nc]) continue;
+
+                costTable[dir][nr][nc] = ncost;
+                q.add(new Point(nr, nc, ncost, dir));
+            }
+        }
+
+        return answer;
+    }
+
+    static class Point {
+        int r, c;
+        int cost;
+        int dir;
+
+        Point(int r, int c, int cost, int dir) {
+            this.r = r;
+            this.c = c;
+            this.cost = cost;
+            this.dir = dir;
+        }
+    }
+}
