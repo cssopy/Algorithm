e366f42a4b80281da4ed93df92b7c54fdf72111a
cssopy <83584721+cssopy@users.noreply.github.com>
2023-06-11 23:39:06 +0900

    Update BJ_15721.java

--- BAEKJOON/SIVER_5/BJ_15721.java
+++ BAEKJOON/SIVER_5/BJ_15721.java
@@ -2,58 +2,59 @@ import java.util.Scanner;
 
 public class BJ_15721 {
     public static void main(String[] args) {
+        int A, T, slogan;
         try (Scanner sc = new Scanner(System.in)) {
-            int A = sc.nextInt();
-            int T = sc.nextInt();
-            int slogan = sc.nextInt();
-
-            // 게임을 시작하고 진행한 사람 수
-            int answer = -1;
-            // 뻔, 데기가 외쳐진 횟수
-            int[] bndNum = {0, 0};
-            for (int N = 1, f = 0; ; N++) {
-                // 뻔 데기 뻔 데기
-                for (int i = 0; i < 2; i++) {
-                    answer++;
-                    bndNum[0]++;
-                    if (bndNum[slogan] == T) {
-                        f = 1;
-                        break;
-                    }
-
-                    answer++;
-                    bndNum[1]++;
-                    if (bndNum[slogan] == T) {
-                        f = 1;
-                        break;
-                    }
-                }
-                if (f == 1) break;
-
-                // 뻔 x N
-                for (int i = 0; i < N + 1; i++) {
-                    answer++;
-                    bndNum[0]++;
-                    if (bndNum[slogan] == T) {
-                        f = 1;
-                        break;
-                    }
-                }
-                if (f == 1) break;
-
-                // 데기 x N
-                for (int i = 0; i < N + 1; i++) {
-                    answer++;
-                    bndNum[1]++;
-                    if (bndNum[slogan] == T) {
-                        f = 1;
-                        break;
-                    }
-                }
-                if (f == 1) break;
-            }
-
-            System.out.println(answer % A);
+            A = sc.nextInt();
+            T = sc.nextInt();
+            slogan = sc.nextInt();
         }
+
+        // 게임을 시작하고 진행한 사람 수
+        int answer = -1;
+        // 뻔, 데기가 외쳐진 횟수
+        int[] bndNum = {0, 0};
+        for (int N = 1, f = 0; ; N++) {
+            // 뻔 데기 뻔 데기
+            for (int i = 0; i < 2; i++) {
+                answer++;
+                bndNum[0]++;
+                if (bndNum[slogan] == T) {
+                    f = 1;
+                    break;
+                }
+
+                answer++;
+                bndNum[1]++;
+                if (bndNum[slogan] == T) {
+                    f = 1;
+                    break;
+                }
+            }
+            if (f == 1) break;
+
+            // 뻔 x N
+            for (int i = 0; i < N + 1; i++) {
+                answer++;
+                bndNum[0]++;
+                if (bndNum[slogan] == T) {
+                    f = 1;
+                    break;
+                }
+            }
+            if (f == 1) break;
+
+            // 데기 x N
+            for (int i = 0; i < N + 1; i++) {
+                answer++;
+                bndNum[1]++;
+                if (bndNum[slogan] == T) {
+                    f = 1;
+                    break;
+                }
+            }
+            if (f == 1) break;
+        }
+
+        System.out.println(answer % A);
     }
 }
